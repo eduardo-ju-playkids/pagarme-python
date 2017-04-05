da09186d81ecbfd24ef5ef7e273bd85a584fdb4c
Victor Messina <victormessina95@hotmail.com>
2017-04-05 10:36:00 -0300

    remove loop and create generic find in payable.py

--- tests/recipient_test.py
+++ tests/recipient_test.py
@@ -33,40 +33,46 @@ class RecipientTestCase(PagarmeTestCase):
     def test_get_recipient_by_id_fail(self):
         recipient = Recipient(api_key='apikey')
         with self.assertRaises(PagarmeApiError):
-            recipient.find_by_id('re_cj01ath3700xqs56dmv00epj6')
+            recipient.find_by_id('re_cj01ath3700xqs56dmv55wpj6')
 
     @mock.patch('requests.get', mock.Mock(side_effect=fake_request)) 
     def test_get_recipient_by_bankAccount_id(self):
         recipient = Recipient(api_key='apikey')
-        recipient.find_by_bank_account_id(17422412)
-        self.assertEqual(17422412, recipient.bank_account['id'])
+        params={"bank_account_id":17422412}
+        recipient.find_by(params)
+        self.assertEqual(params['bank_account_id'], recipient.bank_account['id'])
 
     @mock.patch('requests.get', mock.Mock(side_effect=fake_request_fail_bankAccount_id))
     def test_get_recipient_by_bankAccount_id_fail(self):
         recipient = Recipient(api_key='apikey')
+        params={"bank_account_id":17422413}
         with self.assertRaises(PagarmeApiError):
-            recipient.find_by_bank_account_id(17422412)
+            recipient.find_by(params)
 
     @mock.patch('requests.get', mock.Mock(side_effect=fake_request))
     def test_get_recipient_by_transfer_enabled(self):
         recipient = Recipient(api_key='apikey')
-        recipient.find_by_transfer_enabled(True)
-        self.assertTrue(recipient.transfer_enabled)
+        params={"transfer_enabled":True}
+        recipient.find_by(params)
+        self.assertEqual(params['transfer_enabled'],recipient.transfer_enabled)
 
     @mock.patch('requests.get', mock.Mock(side_effect=fake_request_fail_transfer_enabled))
     def test_get_recipient_by_transfer_enabled_fail(self):
         recipient = Recipient(api_key='apikey')
+        params={"transfer_enabled":False}
         with self.assertRaises(PagarmeApiError):
-            recipient.find_by_transfer_enabled(True)
+            recipient.find_by(params)
 
     @mock.patch('requests.get', mock.Mock(side_effect=fake_request))
     def test_get_recipient_by_transfer_day(self):
         recipient = Recipient(api_key='apikey')
-        recipient.find_by_transfer_day(5)
-        self.assertEqual(5, recipient.transfer_day)
+        params={"transfer_day":5}
+        recipient.find_by(params)
+        self.assertEqual(params['transfer_day'], recipient.transfer_day)
 
     @mock.patch('requests.get', mock.Mock(side_effect=fake_request_fail_transfer_day))
     def test_get_recipient_by_transfer_day_fail(self):
         recipient = Recipient(api_key='apikey')
+        params={"transfer_day":8}
         with self.assertRaises(PagarmeApiError):
-            recipient.find_by_transfer_day(5)
+            recipient.find_by(params)
